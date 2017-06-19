45020779bae59b330aaa9bba9d30fa9bc27a83d5
Woody See <woodyseesk@gmail.com>
2017-06-19 09:05:29 +0800

    added realtimemaps base project files

---
+++ models/Gantry.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const gantrySchema = new mongoose.Schema({
+  gantryID: String,
+  password: String,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+
+  facebook: String,
+  twitter: String,
+  google: String,
+  github: String,
+  instagram: String,
+  linkedin: String,
+  steam: String,
+  tokens: Array,
+
+  profile: {
+    name: String,
+    gender: String,
+    location: String,
+    website: String,
+    picture: String
+  }
+}, { timestamps: true });
