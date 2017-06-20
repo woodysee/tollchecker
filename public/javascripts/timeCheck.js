e0786f877c968f763d2af5c93270d263913e7f7f
Woody See <woodyseesk@gmail.com>
2017-06-20 09:11:28 +0800

    small fix

--- public/javascripts/timeCheck.js
+++ public/javascripts/timeCheck.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
 // Return the name of the weekday today
 
 //Reference to all gantry objects
-var highlightedBlock = new Object{
+var currentBlock = {
   vehicleType: "Passenger Cars/Light Goods Vehicles/Taxis",
   dayType: "Weekdays",
   gantryIDs: [],
@@ -20,41 +20,42 @@ var highlightedBlock = new Object{
   chargeAmount: 3.50
 };
 
+function timeAndDayChecker(startHour,startMins,endHour,endMins) {
+  function isWeekday() {
+    var d = new Date();
+    var dayOfWeek = new Array(7);
 
-function isWeekday() {
-  var d = new Date();
-  var dayOfWeek = new Array(7);
+    dayOfWeek[0] = "Sunday";
+    dayOfWeek[1] = "Weekdays";
+    dayOfWeek[2] = "Weekdays";
+    dayOfWeek[3] = "Weekdays";
+    dayOfWeek[4] = "Weekdays";
+    dayOfWeek[5] = "Weekdays";
+    dayOfWeek[6] = "Saturday";
 
-  dayOfWeek[0] = "Sunday";
-  dayOfWeek[1] = "Weekdays";
-  dayOfWeek[2] = "Weekdays";
-  dayOfWeek[3] = "Weekdays";
-  dayOfWeek[4] = "Weekdays";
-  dayOfWeek[5] = "Weekdays";
-  dayOfWeek[6] = "Saturday";
-
-  var w = dayOfWeek[d.getDay()];
-  return w;
-};
-
-function hourIs() {
-  var d = new Date();
-  var h = d.getHours();
-  return h;
-};
-
-function MinsIs() {
-  var d = new Date();
-  var m = d.getMinutes();
-  return m;
-};
-
-function timeMatch(currentHour,currentMins,startHour,startMins,endHour,endMins) {
-  if (currentHour >= startHour && startMins >= startMins && currentHour <= startHour && currentMins <= startMins) {
-    return true;
-  } else {
-    return false;
+    var w = dayOfWeek[d.getDay()];
+    return w;
   };
-};
 
-return timeMatch(hourIs(),MinsIs(),startHour,startMins,endHour,endMins);
+  function hourIs() {
+    var d = new Date();
+    var h = d.getHours();
+    return h;
+  };
+
+  function MinsIs() {
+    var d = new Date();
+    var m = d.getMinutes();
+    return m;
+  };
+
+  function timeMatch(currentHour,currentMins,startHour,startMins,endHour,endMins) {
+    if (currentHour >= startHour && startMins >= startMins && currentHour <= startHour && currentMins <= startMins) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+  return timeMatch(hourIs(),MinsIs(),startHour,startMins,endHour,endMins);
+};
